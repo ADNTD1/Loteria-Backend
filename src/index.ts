d9eb075535360a39d97407ae9ba73ef3d2a51bae
Daniel Leyva <leyvachavez1912@gmail.com>
2026-09-13 23:57:17 -0700

    refactor: split express app from server startup

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
-import express from 'express';
-import { swaggerSpec } from './config/swagger.config.js';
-import swaggerUi from 'swagger-ui-express'
+import app from './app.js';
 
-import userRouter from './routes/user.routes.js';
-import roomRouter from './routes/room.routes.js';
-import gameRouter from './routes/game.routes.js'
-import { errorHandler, notFoundHandler } from './middlewares/error.middleware.js';
-
-const app = express();
 const PORT = process.env.port || 3000;
-
-
-app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.use('/api/users', userRouter);
-app.use('/api/rooms', roomRouter);
-app.use('/api/game', gameRouter);
-
-// Siempre al final, después de todas las rutas.
-app.use(notFoundHandler);
-app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server listening in port ${PORT}`)
