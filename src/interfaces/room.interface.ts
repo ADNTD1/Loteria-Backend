3ad8587922be495151ec8abb2556598092c3e082
ADNTD1 <adntd24@gmail.com>
2026-09-13 23:45:25 -0700

    feat: Active Room State model

--- src/interfaces/room.interface.ts
+++ src/interfaces/room.interface.ts
@@ -1,3 +1,5 @@
+import type { Card } from "@prisma/client";
+
 export interface Room {
   id: string,
   hostAccountNumber: string,
@@ -11,4 +13,9 @@ export enum RoomStatus {
   WAITING = 'WAITING',
   PLAYING = 'PLAYING',
   FINISHED = 'FINISHED'
+}
+
+export interface ActiveRoomState extends Room {
+  deck: Card[];
+  drawnCards: Card[];
 }
