e0c7a6e5167f0aa49a26dfc645d9465e859bcd3a
ADNTD1 <adntd24@gmail.com>
2026-09-13 21:28:57 -0700

    docs: setup swagger ui documentation for api endpoints

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,22 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Back-Loteria API',
+      version: '1.0.0',
+      description: 'Documentación de la API para el juego de Lotería Mexicana (Salas, Usuarios y Juego)',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor de desarrollo local',
+      },
+    ],
+  },
+
+  apis: ['./src/routes/*.ts', './src/controllers/*.ts'],
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
-import type { Request, Response } from 'express';
+import { swaggerSpec } from './config/swagger.config.js';
+import swaggerUi from 'swagger-ui-express'
+
 import userRouter from './routes/user.routes.js';
 import roomRouter from './routes/room.routes.js';
 import gameRouter from './routes/game.routes.js'
@@ -9,6 +11,7 @@ const PORT = process.env.port || 3000;
 
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api/users', userRouter);
 app.use('/api/rooms', roomRouter);
@@ -18,4 +21,5 @@ app.use('/api/game', gameRouter);
 app.listen(PORT, () => {
   console.log(`server listening in port ${PORT}`)
   console.log(`Server Url: http://localhost:${PORT}`)
+  console.log(`APIs docs in http://localhost:${PORT}/api-docs`)
 })
