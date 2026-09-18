5a7083f82dd100a543e1b5071d09596135cdf186
Karol <annaosro2005@gmail.com>
2026-09-18 00:47:22 -0700

    endpoint agregado para obtener el usuario autenticado + documentación swagger :p

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,17 +1,29 @@
 import { Router } from "express";
-import { loginWithAccountNumber, logoutUser } from "../controllers/user.controller.js";
+import { loginWithAccountNumber, logoutUser, getMe } from "../controllers/user.controller.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
 import rateLimit from "express-rate-limit";
 
 const router = Router();
 
 // POST http://localhost:3000/api/users/login
-router.post("/login", 
-	rateLimit({ windowMs: 60_000,limit: 40,}), // 40 peticiones por minuto, considerando que 38 alumnos usan la misma ip pública
-	loginWithAccountNumber);
+router.post(
+  "/login", 
+  rateLimit({ windowMs: 60_000, limit: 40 }), // 40 peticiones por minuto
+  loginWithAccountNumber
+);
 
 // POST http://localhost:3000/api/users/logout
-router.post("/logout", 
-	rateLimit({ windowMs: 60_000,limit: 50,}),
-	logoutUser);
+router.post(
+  "/logout", 
+  rateLimit({ windowMs: 60_000, limit: 50 }),
+  logoutUser
+);
+
+// GET http://localhost:3000/api/users/me (Protegida con token)
+router.get(
+  "/me",
+  authenticateToken,
+  getMe
+);
 
 export default router;
